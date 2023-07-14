4648f1278618669647fb639db782c91e399d6b89
xongi09 <xongilemaluleke4@gmail.com>
2023-07-14 04:04:27 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program hat prints a line to the standard error
+ * return 1 (success)
+ */
+int main(vold)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
